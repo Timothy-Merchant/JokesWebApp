20d6bc7b02c2c92797a424dcd0a976d9f4df205b
Tim <ryalin1286@gmail.com>
2021-05-04 10:03:17 +0100

    Created a JokesController to handle CRUD for new jokes.

--- JokesWebApp/Models/Joke.cs
+++ JokesWebApp/Models/Joke.cs
@@ -7,7 +7,7 @@ namespace JokesWebApp.Models
 {
     public class Joke
     {
-        public int MyProperty { get; set; }
+        public int Id { get; set; }
         public string JokeQuestion { get; set; }
         public string JokeAnswer { get; set; }
 
